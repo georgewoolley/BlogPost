135f7e923945dd6fd3fe7832e4ff93ddc49cd371
Administrator <administrator@AdminisatorsMBP.fritz.box>
2023-12-15 10:57:16 +0100

    Added nav bar

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3001;
+const port = 3004;
 var postName = "";
 var postDescription = ""; 
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
 
