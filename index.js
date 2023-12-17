a48caefdb7241fa1ec752f132795eb73d43995a0
Administrator <administrator@AdminisatorsMBP.fritz.box>
2023-12-17 18:55:11 +0100

    Updated bootstrap navbar

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3035;
+const port = 3041;
 var postName = "";
 var postDescription = "";
 const postData = [];
